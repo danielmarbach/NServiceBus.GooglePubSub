110ab25c680ac837e8e127da7cbaf7d96555d4c9
danielmarbach <daniel.marbach@openplace.net>
2017-09-08 15:10:51 +0200

    Using publisher client directly

--- src/NServiceBus.GooglePubSub/Dispatcher.cs
+++ src/NServiceBus.GooglePubSub/Dispatcher.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Threading;
+﻿using System.Collections.Generic;
 using System.Threading.Tasks;
 using Google.Cloud.PubSub.V1;
 using Google.Protobuf;
@@ -10,8 +7,6 @@ using NServiceBus.Transport;
 
 class Dispatcher : IDispatchMessages
 {
-    string projectId;
-
     public Dispatcher(string projectId)
     {
         this.projectId = projectId;
@@ -19,42 +14,28 @@ class Dispatcher : IDispatchMessages
 
     public async Task Dispatch(TransportOperations outgoingMessages, TransportTransaction transaction, ContextBag context)
     {
-        var publishers = new ConcurrentStack<SimplePublisher>();
-        try
+        var unicastTransportOperations = outgoingMessages.UnicastTransportOperations;
+        var multicastTransportOperations = outgoingMessages.MulticastTransportOperations;
+
+        var tasks = new List<Task>(unicastTransportOperations.Count + multicastTransportOperations.Count);
+
+        foreach (var operation in unicastTransportOperations)
         {
-            var unicastTransportOperations = outgoingMessages.UnicastTransportOperations;
-            var multicastTransportOperations = outgoingMessages.MulticastTransportOperations;
-            
-            var tasks = new List<Task>(unicastTransportOperations.Count + multicastTransportOperations.Count);
-
-            foreach (var operation in unicastTransportOperations)
-            {
-                tasks.Add(SendMessage(operation, publishers));
-            }
-
-            foreach (var operation in multicastTransportOperations)
-            {
-                tasks.Add(SendMessage(operation, publishers));
-            }
-
-            await (tasks.Count == 1 ? tasks[0] : Task.WhenAll(tasks))
-                .ConfigureAwait(false);
+            tasks.Add(SendMessage(operation));
         }
-        finally
+
+        foreach (var operation in multicastTransportOperations)
         {
-            var stopTasks = new List<Task>(publishers.Count);
-            foreach (var publisher in publishers.ToArray())
-            {
-                stopTasks.Add(publisher.ShutdownAsync(CancellationToken.None));
-            }
-            await (stopTasks.Count == 1 ? stopTasks[0] : Task.WhenAll(stopTasks))
-                .ConfigureAwait(false);
+            tasks.Add(SendMessage(operation));
         }
+
+        await (tasks.Count == 1 ? tasks[0] : Task.WhenAll(tasks))
+            .ConfigureAwait(false);
     }
 
-    async Task SendMessage(UnicastTransportOperation transportOperation, ConcurrentStack<SimplePublisher> publishers)
+    async Task SendMessage(UnicastTransportOperation transportOperation)
     {
-        var simplePublisher = await SimplePublisher.CreateAsync(new TopicName(projectId, transportOperation.Destination));
+        var publisher = await PublisherClient.CreateAsync();
         var message = transportOperation.Message;
         var transportMessage = new PubsubMessage
         {
@@ -62,15 +43,17 @@ class Dispatcher : IDispatchMessages
             Data = ByteString.CopyFrom(message.Body)
         };
         transportMessage.Attributes.Add(message.Headers);
-        await simplePublisher.PublishAsync(transportMessage)
+        await publisher.PublishAsync(new TopicName(projectId, transportOperation.Destination), new[]
+            {
+                transportMessage
+            })
             .ConfigureAwait(false);
-        publishers.Push(simplePublisher);
     }
 
-    async Task SendMessage(MulticastTransportOperation transportOperation, ConcurrentStack<SimplePublisher> publishers)
+    async Task SendMessage(MulticastTransportOperation transportOperation)
     {
         var generateRoutingKey = DefaultRoutingKeyConvention.GenerateRoutingKey(transportOperation.MessageType);
-        var simplePublisher = await SimplePublisher.CreateAsync(new TopicName(projectId, generateRoutingKey));
+        var publisher = await PublisherClient.CreateAsync();
         var message = transportOperation.Message;
         var transportMessage = new PubsubMessage
         {
@@ -78,8 +61,12 @@ class Dispatcher : IDispatchMessages
             Data = ByteString.CopyFrom(message.Body)
         };
         transportMessage.Attributes.Add(message.Headers);
-        await simplePublisher.PublishAsync(transportMessage)
+        await publisher.PublishAsync(new TopicName(projectId, generateRoutingKey), new[]
+            {
+                transportMessage
+            })
             .ConfigureAwait(false);
-        publishers.Push(simplePublisher);
     }
+
+    string projectId;
 }
